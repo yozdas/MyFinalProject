ba096da70617b382ddaab4512025062b53743b6b
yozdas <37111998+yozdas@users.noreply.github.com>
2021-01-31 00:08:01 +0300

    Git Error Fixed

---
+++ Entities/Abstract/IEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Abstract
+{
+    //Ientity implement eden bir class bir veritabanı tablosudur.
+    public interface IEntity
+    {
+    }
+}
