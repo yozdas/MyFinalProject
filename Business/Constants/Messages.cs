ad9d686262ef941845e69d3f6f216058454e38f1
yozdas <37111998+yozdas@users.noreply.github.com>
2021-02-11 00:16:46 +0300

    Exaggerating oop and n-tier

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Constants
+{
+    public static class Messages
+    {
+
+        public static string ProductAdded = "Ürün Eklendi";
+        public static string ProductNameInvalid = "Ürün İsmi geçersiz";
+        public static string ProductsListed = "Ürünler Listelendi";
+        public static string MaintenanceTime = "Bakım Zamanı :)";
+
+
+    }
+}
---
+++ Core/Utilities/Results/Result.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Results
+{
+    public class Result : IResult
+    {
+
+
+        public Result(bool success, string message) : this(success)
+        {
+            //Get ler readonly dir, sadece ctor da set edilebilir.
+            Message = message;
+        }
+        public Result(bool success)
+        {
+            //Get ler readonly dir, sadece ctor da set edilebilir.
+            Success = success;
+
+        }
+
+        public bool Success { get; }
+        public string Message { get; }
+    }
+}
